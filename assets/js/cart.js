80805e98950ca62d0440b4e14a4d1b37eb7b307b
Mark Killian <Killianmo@gmail.com>
2016-02-23 19:42:42 -0600

    uptodateish

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,51 +1,65 @@
 $(document).ready(function(){
-    var products = [
-      {
-        name: "Amazon Fire TV",
-        manufacturer: "Amazoon",
-        imageSrc: "assets/images/product01.jpg",
-        description: "Eligible for free shipping and returns",
-        price: 89.99
-      },
-      {
-        name: "Amazon Fire",
-        manufacturer: "Amazoon",
-        imageSrc: "assets/images/product02.jpg",
-        description: "Eligible for free shipping and returns",
-        price: 69.00
-      },
-      {
-        name: "Amazon Echo",
-        manufacturer: "Amazoon",
-        imageSrc: "assets/images/product03.jpg",
-        description: "Eligible for free shipping and returns",
-        price: 109.99
-      }
-    ];
 
-    var productTemplate = $('template#product').html();
-      for(var i = 0; i < products.length; i++){
-        var newProduct = $(productTemplate);
-        var pData = products[i];
-        
-        $('h2', newProduct).text(products[i].name);
-        var newManufacturer = $('<span/>').addClass('manufacturer').text('by ' + products[i].manufacturer);
-        $('h2', newProduct).append(newManufacturer);
-        $('.image img', newProduct).attr('src', products[i].imageSrc);
-        $('h3', newProduct).text(products[i].description)
-        $('.price', newProduct).text('$' + products[i].price.toFixed(2));
-        $('.mobile-price', newProduct).text('$' + products[i].price.toFixed(2));
-        $(newProduct).data('price', pData.price);
+  $.get('https://www.batchacademy.com/api/wdfne/dummy/amazoon-products', function(products){
+// In here we have access to the data from the server
+  var updateView = function(){
+    var cart = $('.cart .product');
+    var saved = $('.saved .product');
 
-        $('.cart').append(newProduct);
+    if(cart.length === 0){
+      $('.cart .empty').show();
+      $('.cart-total').hide();
+    } else {
+      $('.cart .empty').hide();
+      $('.cart-total').show();
     }
+
+    if(saved.length === 0){
+      $('.saved .empty').show();
+    } else {
+      $('.saved .empty').hide();
+    }
+
+    var cartTotal = 0;
+    $(cart).each(function(){
+      cartTotal += Number($(this).data('price'));
+    });
+
+      $('.cart-total span').text('$', + cartTotal.toFixed(2));
+
+      updateView();  
+
+    };
+
+    var stub = $('template#product').html();
+    
+    for(var i = 0; i < products.length; i++){
+      var newProduct = $(stub);
+      var pData = products[i];
+
+      $('.content h2', newProduct).text(pData.name);
+      var manufacturerSpan = $('<span/>').addClass('manufacturer').text(' by ' + pData.manufacturer);
+      $('.content h2', newProduct).append(manufacturerSpan);
+      $('.content h3', newProduct).text(pData.description);
+      $('.image img', newProduct).attr('src', pData.imageSrc);
+      $('.price, .mobile-price', newProduct).text('$' + pData.price.toFixed(2));
+      $(newProduct).data('price', pData.price);
+
+      $('.cart').append(newProduct);
+    }
+        
     $('.move').on('click',function(){
-      if ($(this).parents().eq(4).hasClass('cart')){
-        $(this).parents().eq(3).appendTo('.saved');
-        $(this).text('Add to cart')
-      } else {
-        $(this).parents().eq(3).appendTo('.cart')
-        $(this).text('Save for later')
-      }
+      if($(this).parents().eq(4).hasClass('cart')){
+      $(this).parents().eq(3).prependTo('.saved');
+      $(this).text('Move to cart')
+    } else {
+      $(this).parents().eg(3).appendTo(.'cart');
+      $(this).text('Save for later');
+    }
+
+    updateView();
+
     });
   });
+// Out here we don't
+    
